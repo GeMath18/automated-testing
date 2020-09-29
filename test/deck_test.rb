4497deb163a97fb48ab4ead2844fabf41108dd24
gessica <gessicamohr18@gmail.com>
2020-09-28 22:34:14 -0700

    Wave 3

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,54 @@
 require_relative 'test_helper'
 
 describe Deck do
+  before do
+    @deck = Deck.new
+  end
   # Write tests for the Deck class.
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  describe "constructor" do
+    it "can be instantiated" do
+      expect(@deck).must_be_instance_of Deck
+    end
+  end
 
+  describe "draw method" do
+    it "must respond to draw method" do
+      expect(@deck).must_respond_to :draw
+    end
+
+    it "returns a Card" do
+      expect(@deck.draw).must_be_instance_of Card
+    end
+
+    it "will let me draw 52 cards from an inital deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+    end
+
+    it "empty deck returns nil" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+
+      expect(@deck.draw).must_be_nil
+    end
+
+  end
+
+  describe "shuffle method" do
+    it "responds to shuffle" do
+      expect(@deck).must_respond_to :shuffle
+    end
+  end
+
+  describe "count method" do
+    it "responds to count" do
+      expect(@deck).must_respond_to :count
+    end
+
+  end
 end
