0a670bfbd6103243a4cc9fa434080552a0efc3d5
gessica <gessicamohr18@gmail.com>
2020-09-28 19:36:22 -0700

    Wave 1

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,16 @@
 
 # card.rb
+#
+# create a constant
+VALID_SUITS = [:hearts, :spades, :clubs, :diamonds].freeze
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    raise ArgumentError, "This an invalid suite" unless VALID_SUITS.include?(suit)
+    raise ArgumentError, "#{value} is an invalid value" if value < 1 || value > 13
+
     @value = value
     @suit = suit
     
