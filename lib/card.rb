27f5276dfed5e435ee8e5d08a1621562f25d40b9
gessica <gessicamohr18@gmail.com>
2020-09-28 21:55:05 -0700

    Wave 2

--- lib/card.rb
+++ lib/card.rb
@@ -18,7 +18,18 @@ class Card
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+
+    if value == 1
+      return "Ace of #{suit.to_s}"
+    elsif  value == 11
+      return "Jack of #{suit.to_s}"
+    elsif  value == 12
+      return "Queen of #{suit.to_s}"
+    elsif value == 13
+      return  "King of #{suit.to_s}"
+    else
+      return "#{value} of #{suit.to_s}"
+    end
   end
 
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -26,6 +26,9 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+      card = Card.new(6, :diamonds)
+      expect(card.to_s).must_equal "6 of diamonds"
+
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -40,6 +43,16 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+      face_cards = {
+          1 => "Ace",
+          11 => "Jack",
+          12 => "Queen",
+          13 => "King"
+      }
+      face_cards.map do |number, face_value|
+      card = Card.new(number, :hearts)
+      expect(card.to_s).must_equal "#{face_value} of hearts"
+      end
     end
   end
 
@@ -47,11 +60,22 @@ describe Card do
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+       [:hearts, :spades, :clubs, :diamonds].each do |suit|
+         (1..13).each do |value|
+           card = Card.new(value, suit)
+           expect(card.value).must_equal value
+         end
+       end
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
-
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          expect(card.suit).must_equal suit
+        end
+      end
     end
   end
 
